2a2a5e8bf4c8beba9e84c7ade054850ef4af3e37
DanielaEsposito <esposito.daniela9817@gmail.com>
2025-03-09 15:57:42 +0100

    add model and default-layout

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,12 @@
+<div class="card h-100">
+
+    <div class="card-body">
+        <h5 class="card-title"></h5>
+        <p class="card-text"></p>
+        <p class="card-text"></p>
+        <p class="card-text"></p>
+        <p class="card-text">
+        </p>
+
+    </div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,9 @@
+@extends('layouts.defaultLayout')
+@section('content')
+    <div class="container">
+        <h1 class="page-title">Tabellone Treni</h1>
+        <div class="row g-3">
 
+        </div>
+    </div>
+@endsection
